a4cf9e6db00f5601d2c7d942006077f70b0c8881
Your Name <you@example.com>
2021-05-27 17:38:34 +0900

    localhost:5000

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ app.debug = True
 @app.route('/', methods=['GET','POST'])
 def hello_world():
     return render_template('Home.html', name = "정호성")
-    
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
 
 if __name__== '__main__':
     app.run(port = 5000)
