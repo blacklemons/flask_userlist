a5e8a72ce9aa4d47d479a0771c2880973b6524fd
Your Name <you@example.com>
2021-05-28 13:30:36 +0900

    add_article

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Articles = Articles()
 
 @app.route('/', methods=['GET','POST'])
 def hello_world():
-    return render_template('Home.html', name = "정호성")
+    return render_template('Home.html')
 
 @app.route('/about')
 def about():
@@ -16,6 +16,20 @@ def about():
 @app.route('/articles')
 def articles():
     return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<id>')
+def article(id):
+    if len(Articles)>=int(id):
+        Article = Articles[int(id)-1]
+        return render_template('article.html', article = Article)
+    else :
+        return render_template('article.html', article = "No DATA")
+
+@app.route('/add_article')
+def add_article():
+    return render_template('add_article.html', articles = Articles)
+
+
 
 if __name__== '__main__':
     app.run(port = 5000)
